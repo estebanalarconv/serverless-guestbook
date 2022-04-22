71395a86aff68e17c943479c7f7b20b76eb95557
estebanalarconv <estebanalarconv@gmail.com>
2022-04-22 11:49:02 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://5c5a82a7.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
